c1939d7302ac33594ad0254db72be784f9de8bcf
vcyzteen <vcyzscape@gmail.com>
2021-12-29 14:12:48 +0700

    fish: move aliases
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
+# interactive
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# fish optimize
 set fish_greeting
 set -U SXHKD_SHELL sh
 set -U SXHKD_SHELL /usr/bin/bash
+
+# load alias
+[ -f ~/.alias ] && . ~/.alias
