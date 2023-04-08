bccc361aff238c49a0513169d9a26f7f592c2f24
ʟᴇᴀ <xealea@proton.me>
2023-04-08 20:34:10 +0700

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,7 +1,4 @@
-sudo xbps-install -S \
-     xorg xorg-server bspwm sxhkd ncmpcpp feh cava mpd mpc xdo \
-     fish-shell xsettingsd dunst mpv neofetch neovim rofi viewnior xfce4-power-manager \
-     nautilus ranger fontconfig rsync wget curl w3m w3m-img ImageMagick pamixer pavucontrol \
-     scrot polkit xfce-polkit xdg-user-dirs youtube-dl alacritty screenkey xautolock i3lock-color ueberzug \
-     xfce4-power-manager-settings xscreensaver pulseaudio pulseeffects-legacy wmname skyppy-xd slop tint2 polybar \
-     NetworkManagaer xsettingsd gtk4 gtk3 gtk2
+rsync -avxHAXP --exclude '.git*' .* ~/ \
+     chsh -s /usr/bin/fish \
+     pushd ~/.icons/ && sudo ln -vs ~/.icons/Tela-grey /usr/share/icons/Tela-grey && popd \
+     fc-cache -rv
