2530a415d22e844bb6042c68cfce6e87889e5349
ʟᴇᴀ <xealea@proton.me>
2023-04-08 20:22:00 +0700

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,7 @@
+sudo xbps-install -S \
+     xorg xorg-server bspwm sxhkd ncmpcpp feh cava mpd mpc xdo \
+     fish-shell xsettingsd dunst mpv neofetch neovim rofi viewnior xfce4-power-manager \
+     nautilus ranger fontconfig rsync wget curl w3m w3m-img ImageMagick pamixer pavucontrol \
+     scrot polkit xfce-polkit xdg-user-dirs youtube-dl alacritty screenkey xautolock i3lock-color ueberzug \
+     xfce4-power-manager-settings xscreensaver pulseaudio pulseeffects-legacy wmname skyppy-xd slop tint2 polybar \
+     NetworkManagaer xsettingsd gtk4 gtk3 gtk2
